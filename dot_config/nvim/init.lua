d25054bf10adad4c167442635f33123ecf1dda2e
sandeshbhusal <mail.sandeshbhusal@gmail.com>
2026-06-14 11:46:34 -0400

    vimrc things
    Signed-off-by: sandeshbhusal <mail.sandeshbhusal@gmail.com>

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,5 @@
+vim.opt.number = true       -- line numbers
+vim.opt.tabstop = 4         -- a tab displays as 4 columns
+vim.opt.shiftwidth = 4      -- >> / << and autoindent use 4
+vim.opt.softtabstop = 4     -- Tab key inserts 4 spaces' worth
+vim.opt.expandtab = true    -- insert spaces, never a literal tab
